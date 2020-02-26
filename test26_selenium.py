65703715041b57a6bbe6e79a6c8640019020048c
quyixiao <2621048238@qq.com>
2020-02-26 16:56:23 +0800

    提交修改

--- test26_selenium.py
+++ test26_selenium.py
@@ -9,6 +9,14 @@ from selenium.webdriver.support.wait import WebDriverWait
 # expected_conditions条件，负责条件触发
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
+# 打开URL,可以跟踪跳转，可以返回当前页面的实际URL
+# 获取页面的title
+# 处理cookie
+# 控制浏览器操作，例如前进，后退，刷新，关闭，最大化等
+# 执行js脚本
+# 在DOM 中搜索页面元素WEb Elment，指定或者一批，find系方法
+# 操作网页元素
+# 模似下拉框
 
 # 页面隐式等待
 driver = webdriver.PhantomJS(
