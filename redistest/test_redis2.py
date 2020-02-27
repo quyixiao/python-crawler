b58e3afb68ac49dbdb12766e752d3cf5258f2645
quyixiao <2621048238@qq.com>
2020-02-27 09:36:20 +0800

    提交修改

--- redistest/test_redis2.py
+++ redistest/test_redis2.py
@@ -1,15 +1,15 @@
 import redis
 
-r = redis.Redis(host='172.16.157.238', password='Hello1234', port=6379, db=9)
+r = redis.Redis(host='localhost',port=6379)
 
-r.zadd('mboard', 'yellow', 1, 'rolling in the deep')
+r.zadd('mboard', {'yellow': 1, 'rolling in the deep':2})
 
-r.zadd('mboard', 'eye of the tiger', 1, 'billie jean', 1, 'say you say me', 1, 'payphone', 1)
-r.zadd('mboard', 'my heart will go on', 1, 'when you believe', 1, 'hero', 1)
-r.zincrby('mboard', 'yellow', 50)
-r.zincrby('mboard', 'rolling in the deep', 60)
-r.zincrby('mboard', 'my heart will go on', 68.8)
-r.zincrby('mboard', 'when you believe', 70)
+r.zadd('mboard', {'eye of the tiger': 1, 'billie jean': 1, 'say you say me':1, 'payphone':1})
+r.zadd('mboard', {'my heart will go on': 1, 'when you believe': 1, 'hero': 1})
+r.zincrby('mboard', 50,'yellow')
+r.zincrby('mboard', 60,'rolling in the deep' )
+r.zincrby('mboard', 68.8, 'my heart will go on')
+r.zincrby('mboard', 70,'when you believe' )
 # 所有元素
 allmusic = r.zrange('mboard', 0, -1, withscores=True)
 print(type(allmusic))
